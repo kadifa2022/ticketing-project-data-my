af4cb750ede1c036daf0e97b4ce49606a0e34fff
Kadifa <Kudickadifa@gmail.com>
2022-11-01 12:17:39 -0500

    ticketing project data

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface UserService {
 
-    List<UserDTO> listAllUser();
+    List<UserDTO> listAllUsers();
     UserDTO findByUserName(String username);
     void save(UserDTO user);
     void deleteByUserName(String username);

b9e0a8de4da2fb74a190d4b91a6e3470a7228eb4
Kadifa <Kudickadifa@gmail.com>
2022-11-04 13:51:28 -0500

    ticketing project data

---
+++ src/main/java/com/cydeo/mapper/ProjectMapper.java
@@ -0,0 +1,29 @@
+package com.cydeo.mapper;
+
+import com.cydeo.converter.ProjectDtoConverter;
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.entity.Project;
+
+import org.modelmapper.ModelMapper;
+
+public class ProjectMapper {
+
+    private ModelMapper modelMapper;                //solid principle do not crete all in one class-- create methode whatever we need use
+
+    public ProjectMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+
+    public Project convertToEntity(ProjectDTO dto) {
+
+        return modelMapper.map(dto, Project.class);//entity class
+
+    }
+
+    public ProjectDTO convertToDto(Project entity) {
+        return modelMapper.map(entity, ProjectDTO.class);
+    }
+}
+
+

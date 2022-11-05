bdf51f8d336aa8d0147d0d9b6e0cf577def23466
Kadifa <Kudickadifa@gmail.com>
2022-11-04 20:02:01 -0500

    ticketing project data

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -3,11 +3,15 @@ package com.cydeo.repository;
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import javax.transaction.Transactional;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     
     User findByUserName(String username);
     @Transactional//if transaction is successful will get commit if not rollback
     void deleteByUserName(String username);//derived query
+
+    List<User> findByRoleDescriptionIgnoreCase(String description);
 }

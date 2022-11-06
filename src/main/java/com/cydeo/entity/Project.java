932dc934ff02a7661a928bfb6942753ec45ffb3b
Kadifa <Kudickadifa@gmail.com>
2022-11-06 17:35:48 -0600

    ticketing project data

--- src/main/java/com/cydeo/entity/Project.java
+++ src/main/java/com/cydeo/entity/Project.java
@@ -1,7 +1,7 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.Status;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,13 +13,14 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name= "projects")
-@Where(clause="is_deleted=false")
+@Where(clause= "is_deleted=false")
 public class Project extends BaseEntity{
 
+    @Column(unique = true)
     private String projectCode;
+
     private String projectName;
 
     @Column(columnDefinition = "DATE")

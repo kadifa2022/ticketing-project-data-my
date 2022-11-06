3f2763c2ef8250c8721d2e85ade17b9a3f931029
Kadifa <Kudickadifa@gmail.com>
2022-11-06 17:36:57 -0600

    ticketing project data

---
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -0,0 +1,22 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
+
+import java.util.List;
+
+public interface TaskService {
+
+
+    List<TaskDTO> listAllTasks();
+    void save(TaskDTO dto);
+    void update(TaskDTO dto);
+    void delete (Long id);//we don't have nothing uniq here that's why using id
+
+    TaskDTO findById(Long id);
+
+    int totalNonCompletedTask(String projectCode);
+    int totalCompletedTask(String projectCode);
+
+
+}

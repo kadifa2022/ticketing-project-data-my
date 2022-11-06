68e3313466f2fad6fc9cce60cac40a113225e3ff
Kadifa <Kudickadifa@gmail.com>
2022-11-06 17:36:21 -0600

    ticketing project data

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,12 +1,16 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.Project;
 import com.cydeo.entity.User;
 import com.cydeo.enums.Status;
 import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.ProjectRepository;
 import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +22,16 @@ public class ProjectServiceImpl implements ProjectService {
 
     private final ProjectRepository projectRepository;
     private final ProjectMapper projectMapper;
+    private final UserService userService;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+    private final UserMapper userMapper;
+    private final TaskService taskService;
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper, UserService userService, UserMapper userMapper, TaskService taskService) {
         this.projectRepository = projectRepository;
         this.projectMapper = projectMapper;
+        this.userService = userService;
+        this.userMapper = userMapper;
+        this.taskService = taskService;
     }
 
     @Override
@@ -70,5 +80,26 @@ public class ProjectServiceImpl implements ProjectService {
         project.setProjectStatus(Status.COMPLETE);
         projectRepository.save(project);
 
+    }
+
+    @Override
+    public List<ProjectDTO> listAllProjectDetails() {
+
+      UserDTO currentUserDTO=userService.findByUserName("harold@manager.com");// from security
+      User user = userMapper.convertToEntity(currentUserDTO);
+      List<Project> list =projectRepository.findAllByAssignedManager(user);
+
+        return list.stream().map(project -> {
+
+            ProjectDTO obj=projectMapper.convertToDto(project);//converte to list
+
+            obj.setUnfinishedTaskCounts(taskService.totalNonCompletedTask(project.getProjectCode()));//set up fields
+            obj.setCompleteTaskCounts(taskService.totalCompletedTask(project.getProjectCode()));
+
+            return  obj;
+                }
+                ).collect(Collectors.toList());
+
+        //hey db, give me all projects assigned to manager login in the system
     }
 }

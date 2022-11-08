1049676429a031a1b7244beb185fc9d9a8d0ff69
Kadifa <Kudickadifa@gmail.com>
2022-11-07 20:47:46 -0600

    ticketing project data

--- src/main/java/com/cydeo/service/TaskService.java
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -2,6 +2,8 @@ package com.cydeo.service;
 
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Status;
 
 import java.util.List;
 
@@ -19,6 +21,16 @@ public interface TaskService {
     int totalCompletedTask(String projectCode);
 
     void deleteByProject(ProjectDTO projectDTO);
+
+    void completeByProject(ProjectDTO projectDTO);
+    List<TaskDTO>listAllTasksByStatusIsNot(Status status);
+    List<TaskDTO>listAllTasksByStatus(Status status);
+
+    List<TaskDTO> listAllNonCompletedByAssignedEmployee(UserDTO assignedEmployee);
+
+
+
+
 
 
 }

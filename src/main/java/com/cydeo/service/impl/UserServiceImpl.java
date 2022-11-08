cb617146b95f129268fc17a816a704ebaf6b7c77
Kadifa <Kudickadifa@gmail.com>
2022-11-07 20:47:59 -0600

    ticketing project data

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
 import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +21,17 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final ProjectService projectService;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    private final TaskService taskService;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, @Lazy ProjectService projectService, @Lazy TaskService taskService) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.projectService = projectService;
+        this.taskService = taskService;
     }
+
     @Override
     public List<UserDTO> listAllUsers() {
        List<User> userList= userRepository.findAll(Sort.by("firstName"));
@@ -69,13 +80,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public void delete(String username) {
         User user = userRepository.findByUserName(username);
-        user.setIsDeleted(true);
-        userRepository.save(user);//from user service update and never delete
+        if(checkIfUserCanBeDeleted(user)) { //condition
+            user.setIsDeleted(true);
+            userRepository.save(user);//from user service update and never delete
 
-        //logic go to Db and het that user with username
-        //change the isDeleted field dto true
-        //save the object in db
-
+            //logic go to Db and het that user with username
+            //change the isDeleted field dto true
+            //save the object in db
+        }
     }
 
     @Override
@@ -84,5 +96,21 @@ public class UserServiceImpl implements UserService {
         List<User> users = userRepository.findByRoleDescriptionIgnoreCase(role);
 
         return users.stream().map(userMapper::convertToDto).collect(Collectors.toList());
+    }
+
+    private boolean checkIfUserCanBeDeleted(User user) {
+
+        switch (user.getRole().getDescription()) {
+            case "Manager":
+                List<ProjectDTO> projectDTOList = projectService.
+                        listAllNonCompletedByAssignedManager(userMapper.convertToDto(user));
+                return projectDTOList.size() == 0;//true
+            case "Employee":
+                List<TaskDTO> taskDTOList = taskService.listAllNonCompletedByAssignedEmployee(userMapper.convertToDto(user));
+                return taskDTOList.size() == 0;
+            default:
+                return true;
+        }
+
     }
 }

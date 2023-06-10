df0f9e715c74f79f2574bcef77fa45a5f99cc010
Kadifa <Kudickadifa@gmail.com>
2023-06-10 14:27:27 -0500

    ticketing project data

--- src/main/java/com/cydeo/mapper/TaskMapper.java
+++ src/main/java/com/cydeo/mapper/TaskMapper.java
@@ -1,8 +1,8 @@
 package com.cydeo.mapper;
 
-import com.cydeo.dto.RoleDTO;
+
 import com.cydeo.dto.TaskDTO;
-import com.cydeo.entity.Role;
+
 import com.cydeo.entity.Task;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
